e635a8841bf3ddcee63ff422b1e192421a80da27
Shilpi <bhattacharya.shilpi@yahoo.com>
2019-01-22 08:01:17 +0530

    lazy loading

--- FirstFormAngular/src/app/app-routing.module.ts
+++ FirstFormAngular/src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ListEmployeeComponent } from './employee/list-employee.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ContactFormComponent } from './contact/contact-form.component';
+
 
 const routes: Routes = [
-  {path: '', redirectTo:'/list', pathMatch:'full'},
-  {path:'employee', children:[
-    {path: 'contact', component:ContactFormComponent},
-    {path:'list', component:ListEmployeeComponent},
-    {path:'create', component:CreateEmployeeComponent},
-  ]} 
+
+  { path: '', redirectTo: '/list', pathMatch: 'full' }, 
+  // // {path:'employee', children:[
+  // { path: 'contact', component: ContactFormComponent },
+  // { path: 'list', component: ListEmployeeComponent },
+  // { path: 'create', component: CreateEmployeeComponent },
+  // // ]} 
 ];
 
 @NgModule({
